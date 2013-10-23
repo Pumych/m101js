700a9d4e28a4845abe1a36186cdb5dd97cf20984
Pumych <pumych@gmail.com>
2013-10-23 15:48:43 +0300

    Hello World using Express ans Swig

--- week1/hello_express.js
+++ week1/hello_express.js
@@ -1,6 +1,22 @@
 var express = require('express'),
-	app = express();
+	app = express()
+	consolidate = require('consolidate');
 
-app.get('/', function(){
+app.engine('html', consolidate.swig);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
+
+// req - request
+// res - result/response
+app.get('/', function(req, res){
+	res.render('hello', {'name': 'Swig'});
 });
+
+app.get('*', function(req, res){
+	res.send('WOW 404!', 404);
+});
+
+app.listen(8000);
+
+console.log('Express server started on port 8000');
